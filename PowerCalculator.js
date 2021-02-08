9723d53262ccd9e1445916aaa945ccb1d48492a0
Danny Carcamo <dannycarcamo@icloud.com>
2021-02-08 14:50:33 -0600

    4th drill complete

--- PowerCalculator.js
+++ PowerCalculator.js
@@ -1,4 +1,5 @@
 /* eslint-disable strict */
+
 /* Write a function called powerCalculator() that takes two parameters, an integer as a base, and another integer as an exponent. The function returns the value of the base raised to the power of the exponent. Use only exponents greater than or equal to 0 (positive numbers) */
 
 function powerCalculator(num, exponent) {
--- ReverseString.js
+++ ReverseString.js
@@ -1,4 +1,5 @@
 /* eslint-disable strict */
+
 /* Write a function that reverses a string. Take a string as input, reverse the string, and return the new string. */
 
 function reverseString(str) {
