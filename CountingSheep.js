fe136be5b1292d516ecede39d86c36d5cbc05087
Danny Carcamo <dannycarcamo@icloud.com>
2021-02-08 11:48:43 -0600

    first drill complete

---
+++ CountingSheep.js
@@ -0,0 +1,10 @@
+/* eslint-disable strict */
+function countSheep(num) {
+  if (num === 0) {
+    return console.log('All sheep jumped over the fence');
+  }
+  console.log(num + ': Another sheep jumps over the fence');
+  countSheep(num - 1);
+}
+
+countSheep(3);
