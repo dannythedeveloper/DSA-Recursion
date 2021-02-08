aec16f3b6f85e026b9110984b8970f073985a352
Danny Carcamo <dannycarcamo@icloud.com>
2021-02-08 12:04:01 -0600

    second drill complete

---
+++ PowerCalculator.js
@@ -0,0 +1,16 @@
+/* eslint-disable strict */
+/* Write a function called powerCalculator() that takes two parameters, an integer as a base, and another integer as an exponent. The function returns the value of the base raised to the power of the exponent. Use only exponents greater than or equal to 0 (positive numbers) */
+
+function powerCalculator(num, exponent) {
+  if (exponent === 0 ) {
+    return 1;
+  }
+  if (exponent < 0) {
+    return 'exponent should be >= 0';
+  } else {
+    return num * powerCalculator(num, exponent - 1);
+  }
+}
+
+console.log(powerCalculator(10, 2)); // 100
+console.log(powerCalculator(2, 3)); // 8
