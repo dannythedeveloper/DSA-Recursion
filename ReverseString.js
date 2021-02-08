31c692a0046348f49adee3b7109d9989b2fdb6d8
Danny Carcamo <dannycarcamo@icloud.com>
2021-02-08 12:19:47 -0600

    third drill complete

---
+++ ReverseString.js
@@ -0,0 +1,12 @@
+/* eslint-disable strict */
+/* Write a function that reverses a string. Take a string as input, reverse the string, and return the new string. */
+
+function reverseString(str) {
+  if (str === '') {
+    return '';
+  }
+  return reverseString(str.slice(1)) + str[0];
+}
+
+console.log(reverseString('hello')); // olleh
+console.log(reverseString('javascript')); // tpircsavaj
